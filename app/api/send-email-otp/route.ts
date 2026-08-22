10193ab43a0a3d7c39dd8ef4a567baaf23b88779
hirunlakshith04-ctrl <hirunlakshitha04@gmail.com>
2026-08-22 05:38:24 +0530

    new

--- app/api/send-email-otp/route.ts
+++ app/api/send-email-otp/route.ts
@@ -58,8 +58,8 @@ export async function POST(req: Request) {
       port: 465,
       secure: true, // true for port 465 (SSL)
       auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS,
+        user: process.env.EMAIL_SERVER_USER,
+        pass: process.env.EMAIL_SERVER_PASSWORD,
       },
     });
 
